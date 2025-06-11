305b8faf68609e7336ee22bc319f6fa35ac5f85d
Ayelen may <Ayelen.kirchhoff@gmail.com>
2025-06-11 15:41:11 +0200

    game_logic finished

--- game_logic.py
+++ game_logic.py
@@ -1,16 +1,18 @@
 import random
 from ascii_art import STAGES
 
-# List of secret words
+"""List of secret words"""
 WORDS = ["python", "git", "github", "snowman", "meltdown"]
 max_attempts = len(STAGES) -1
 
+
+"""Selects a random word from the list."""
 def get_random_word():
-    """Selects a random word from the list."""
     return WORDS[random.randint(0, len(WORDS) - 1)]
 
+""" Displays stages of snowman for current mistakes"""
+""" Display guessed letter + mistakes count"""
 def  display_game_state(mistakes, secret_word, guessed_letters):
-    """ Displays stages of snowman for current mistakes"""
     print(STAGES[mistakes])
     display_word = ""
 
@@ -31,9 +33,8 @@ def play_game():
     attempts = 0
 
     print("Welcome to Snowman Meltdown!")
-   # print("Secret word selected: " + secret_word)  # for testing, later remove this line
+    print("Secret word selected: " + secret_word)  # for testing, later remove this line
 
-    # TODO: Build your game loop here.
     """ Game Loop"""
     while attempts < max_attempts:
         display_game_state(attempts, secret_word, guessed_letter)
@@ -42,33 +43,30 @@ def play_game():
             print("You guessed the word! you win ")
             break
 
+        while True:
+            guess = input("Guess a letter: ").lower()
+            if len(guess) != 1 or guess not in "abcdefghijklmnopqrstuvwxyz":
+                print("please enter a single alphabetical letter (a -z).")
+            elif guess in guessed_letter:
+                print("You already guess that letter, try again!")
+            else:
+                break
 
-        guess = input("Guess a letter: ").lower()
-        print("You guessed:", guess)
-
-        if guess in guessed_letter:
-            print("You already guess that letter, try again!")
-            continue
-
+        """Correct guesses added to guessed_letters"""
+        """Runs if input is valid"""
         guessed_letter.add(guess)
 
+        """Mistakes counter for incorrect guess"""
         if guess not in secret_word:
             attempts += 1
             print(f"Incorrect, try again!")
-
         else:
             print("Nice Guess!")
-
-        if all(letter in guessed_letter for letter in secret_word):
-            print("You saved the Snowman! The word was:", secret_word)
-            break
-
     else:
-        display_game_state(attempts, secret_word, guessed_letter)
-        print(f"You are all out of guesses and the Snowman is melted. The word was: {secret_word}")
+         display_game_state(attempts, secret_word, guessed_letter)
+         print(f"You are all out of guess and the snowman is melted")
 
 
-    # print("would you like to play again:"(y/n))
 
 if __name__ == "__main__":
     play_game()
